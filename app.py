7a4bd335298896d53859868e1946db54742933b7
CSEcass <cassidy.reyes@csedge.org>
2024-07-25 12:02:10 -0700

    Many changes, login info works, reordered pages, added pages

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 # Basic setup ^^^^
 
-# class
-
+# globally stored user input(From sign_in page)
 class USER:
     def __init__ (self, fname = None, lname = "Re", email = None):
         self.fname = fname
@@ -12,13 +11,18 @@ class USER:
         self.email = email
 user = USER()
 
-@app.route('/')
-def HomePage():
-    return render_template('NOT_SIGNED_IN/index.html')
-
+# >>>NOT_SIGNED_IN pages:
 @app.route('/complain')
 def Complain():
     return render_template('NOT_SIGNED_IN/complain.html')
+
+@app.route('/shop')
+def Shop():
+    return render_template('NOT_SIGNED_IN/index.html')
+
+@app.route('/')
+def HomePage():
+    return render_template('NOT_SIGNED_IN/info.html')
 
 @app.route('/ordering1')
 def Ordering():
@@ -27,32 +31,37 @@ def Ordering():
 @app.route('/sign_up')
 def SignUp():
     return render_template('NOT_SIGNED_IN/sign_up.html',url=url_for('SignInForm'))
-# FORM FILLOUT vvvv
-
-# Completed.html(Cass)
+    # FORM FILLOUT vvvv
 @app.route('/SIGNED_IN/index2', methods=['GET', 'POST'])
 def SignInForm():
     if request.method == 'POST':
         fname = request.form['fname']
         lname = request.form['lname']
         email = request.form['email']
+        global user
         user = USER(fname, lname, email)
     return render_template('SIGNED_IN/index2.html', fname=user.fname, lname = user.lname, 
                            email = user.email)
-    
-
-@app.route('/2')
-def SIHomePage():
-    return render_template('SIGNED_IN/index2.html', fname = user.fname, lname = user.lname, email = user.email)
-
+# >>>SIGNED_IN pages(Same order in templates folder):
 @app.route('/contact')
 def SIContact():
     return render_template('SIGNED_IN/contact.html')
+
+@app.route('/shop2')
+def SIShop():
+    return render_template('SIGNED_IN/index2.html', fname = user.fname, lname = user.lname, email = user.email)
+
+@app.route('/2')
+def SIHomePage():
+    return render_template('SIGNED_IN/info2.html')
 
 @app.route('/ordering2')
 def SIOrdering():
     return render_template('SIGNED_IN/ordering2.html')
 
+@app.route('/profile')
+def SIProfile():
+    return render_template('SIGNED_IN/profile.html')
 
 # Basic setup vvvvv
 if __name__ == "__main__":
